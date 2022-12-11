00627a998e4b6c0cd78c48d6d8a9433f730f8d1f
Julia Alekseeva <soifka@gmail.com>
2022-12-11 20:03:30 +0200

    JSX syntax

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -24,10 +24,26 @@ class Counter extends React.Component {
     }
 
     render() {
-        const h2 = React.createElement('h2', {}, this.state.count);
-        const buttonPlus = React.createElement('button', {title: 'Plus 1', onClick: () => {this.increment()}}, 'Increment');
-        const buttonMinus = React.createElement('button', {title: 'Minus 1', onClick: () => {this.decrement()}}, 'Decrement');
-        return React.createElement(React.Fragment, {}, h2, buttonMinus, buttonPlus);
+        // const h2 = React.createElement('h2', {title: 'Hello'}, this.state.count);
+        // const buttonPlus = React.createElement('button', {title: 'Plus 1', onClick: () => {this.increment()}}, 'Increment');
+        // const buttonMinus = React.createElement('button', {title: 'Minus 1', onClick: () => {this.decrement()}}, 'Decrement');
+        // return React.createElement(React.Fragment, {}, h2, buttonMinus, buttonPlus);
+
+        
+        // Так тоже работает)))
+        // const h2 = <h2 title = "Hello">{this.state.count}</h2>;
+        // const buttonMinus = <button title = "Minus 1" onClick = {() => {this.decrement()}}>Decrement</button>;
+        // const buttonPlus = <button title = "Plus 1" onClick = {() => {this.increment()}}>Increment</button>;
+        // return React.createElement(React.Fragment, {}, h2, buttonMinus, buttonPlus);
+
+
+        /////// Синтаксис JSX ///////
+        return (<React.Fragment> 
+            <h2 title = "Hello">{this.state.count}</h2>
+            <button title = "Minus 1" onClick = {() => {this.decrement()}}>Decrement</button>
+            <button title = "Plus 1" onClick = {() => {this.increment()}}>Increment</button>
+        </React.Fragment>)
+        // Вместо <React.Fragment></React.Fragment> можно оставить пустые угловые скобки, это тоже будет реакт фрагмент, и будет работать
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -1,15 +1,21 @@
-import { fromByteArray } from "ipaddr.js";
 import React from "react";
 import ReactDOM from "react-dom";
+import { createRoot } from "react-dom/client"; // для версии с JSX кодом
 import Counter from "./components/Counter";
 import * as Math from "./components/Math"; 
 /*
-    * as  значит, что экспортируется все, что есть в Math.js, при этом все импортированные функции
-группируются в некий Math
+    * as в строке 5 значит, что экспортируются все функции, которые есть в Math.js, 
+при этом все эти функции группируются в некий Math
 */
 
-const component = React.createElement(Counter);
-const root = document.querySelector('#root');
-ReactDOM.render(component, root);
+// const component = React.createElement(Counter);
+// const root = document.querySelector('#root');
+// ReactDOM.render(component, root);
+
+
+///// Для версии с JSX кодом ///////
+const root = createRoot(document.querySelector('#root'));
+root.render(<Counter />);
+////////////////////////
 
 console.log(Math.sum(2, 5));
